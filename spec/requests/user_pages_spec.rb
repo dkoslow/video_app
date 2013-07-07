aad7e18f4b1df1f76f7153c18b94af63a856dc0a
Dan Koslow <dan.koslow@gmail.com>
2013-07-07 19:13:29 -0400

    Add basic user signup specs and get to green.
    Add strong parameters to users controller. Implement initial new and create actions.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,6 +7,26 @@ describe "User pages" do
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content('Sign up' ) }
+    let(:submit_form) { "Sign up!" }
+
+    describe "without valid information" do
+      it "should not change the user count" do
+        expect { click_button submit_form }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "First name",   with: "Sarah"
+        fill_in "Last name",    with: "McMara"
+        fill_in "Email",        with: "sarah@mcmara.com"
+        fill_in "Password",     with: "password123"
+        fill_in "Confirmation", with: "password123"
+      end
+
+      it "should create a new user" do
+        expect { click_button submit_form }.to change(User, :count).by(1)
+      end
+    end
   end
 end
