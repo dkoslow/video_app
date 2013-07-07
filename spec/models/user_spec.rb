ad72366665cefb043067767b83901fba7b405873
Dan Koslow <dan.koslow@gmail.com>
2013-07-07 19:15:57 -0400

    Update formatting for user model spec.
    Add a describe block that wraps all of the "User attributes" specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,53 +19,54 @@ describe User do
 
   it { should be_valid }
 
-  describe "when first name is not present" do
-    before { @user.first_name = " "}
-    it { should_not be_valid }
-  end
+  describe "User attributes" do
 
-  describe "when last name is not present" do
-    before { @user.last_name = " "}
-    it { should_not be_valid }
-  end
-
-  describe "when email address is not present" do
-    before { @user.email = " " }
-    it { should_not be_valid }
-  end
-
-  describe "when email address in not unique" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.email.upcase!
-      user_with_same_email.save
+    describe "when first name is not present" do
+      before { @user.first_name = " "}
+      it { should_not be_valid }
     end
 
-    it { should_not be_valid }
-  end
-
-  describe "when password is blank" do
-    before do
-      @user.password = " "
-      @user.password_confirmation = " "
+    describe "when last name is not present" do
+      before { @user.last_name = " "}
+      it { should_not be_valid }
     end
 
-    it { should_not be_valid }
-  end
-
-  describe "when password is too short" do
-    before do
-      @user.password = "foo"
-      @user.password_confirmation = "foo"
+    describe "when email address is not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
     end
 
-    it { should_not be_valid }
+    describe "when email address in not unique" do
+      before do
+        user_with_same_email = @user.dup
+        user_with_same_email.email.upcase!
+        user_with_same_email.save
+      end
+
+      it { should_not be_valid }
+    end
+
+    describe "when password is blank" do
+      before do
+        @user.password = " "
+        @user.password_confirmation = " "
+      end
+
+      it { should_not be_valid }
+    end
+
+    describe "when password is too short" do
+      before do
+        @user.password = "foo"
+        @user.password_confirmation = "foo"
+      end
+
+      it { should_not be_valid }
+    end
+
+    describe "when password and password_confirmation don't match" do
+      before { @user.password_confirmation = "foobarfoo"}
+      it { should_not be_valid }
+    end
   end
-
-  describe "when password and password_confirmation don't match" do
-    before { @user.password_confirmation = "foobarfoo"}
-    it { should_not be_valid }
-  end
-
-
 end
