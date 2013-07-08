7ed58418631d8d59916db463bbcf12a156b22a4f
Dan Koslow <dan.koslow@gmail.com>
2013-07-07 20:14:36 -0400

    Add sessions resource to routes file and add 'signup', 'signing', and 'signout' matches. Also, point root to signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 EducationalVideoApp::Application.routes.draw do
   resources :users, only: [:show, :new, :create, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/signup', to: 'users#new', via: 'get'
+  root to: 'users#new'
+
+  match '/signup',  to: 'users#new',        via: 'get'
+  match '/signin',  to: 'sessions#new',     via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
