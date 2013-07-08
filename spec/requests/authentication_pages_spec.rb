d34984e9bffc2e04e4df7d3eff58f47493e2ef37
Dan Koslow <dan.koslow@gmail.com>
2013-07-07 20:12:23 -0400

    Create sessions controller and initial signin page and specs.
    Implementation current doesn't include any signin functionality, only signin page and page specs.

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Authentication pages" do
+
+  subject { page }
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "without valid credentials" do
+      before { click_button "Sign in" }
+
+      it { should have_error_message('Invalid') }
+    end
+
+    describe "with valid credentials" do
+      user = User.create(first_name: "John", last_name: "Doe",
+                       email: "john@example.com", password: "ohaihai",
+                       password_confirmation: "ohaihai")
+      before do
+        fill_in "Email",    with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_link("Sign out", href: signout_path) }
+
+      describe "signout after signin" do
+        before { click_link "Sign out" }
+        it { should have_link("Sign in") }
+      end
+    end
+  end
+end
